3a3b9a56bebabfcb1a269cc42c523e7c742fb655
Daniel Roe <daniel@roe.dev>
2022-10-15 11:45:02 +0100

    style: lint

--- src/module.ts
+++ src/module.ts
@@ -10,7 +10,7 @@ import { joinURL, parseURL } from 'ufo'
 import { BundleBuilder } from 'wbn'
 
 export interface ModuleOptions {
-  baseURL: string,
+  baseURL: string
   formatVersion: string
   primaryURL?: string
   filename: string
@@ -24,9 +24,9 @@ export default defineNuxtModule<ModuleOptions>({
   defaults: nuxt => ({
     baseURL: joinURL('http://localhost:3000', nuxt.options.app.baseURL),
     formatVersion: 'b2',
-    filename: 'bundle.wbn'
+    filename: 'bundle.wbn',
   }),
-  async setup (options, nuxt) {
+  async setup(options, nuxt) {
     // Skip when preparing
     if (nuxt.options._prepare) return
 
@@ -35,13 +35,16 @@ export default defineNuxtModule<ModuleOptions>({
     nuxt.hook('nitro:config', config => {
       config.prerender ||= {}
       config.prerender.crawlLinks = true
-      config.prerender.routes = [...config.prerender.routes || [], nuxt.options.ssr ? '/' : '/index.html'].filter(i => i !== '/200.html')
+      config.prerender.routes = [
+        ...(config.prerender.routes || []),
+        nuxt.options.ssr ? '/' : '/index.html',
+      ].filter(i => i !== '/200.html')
     })
 
     const themeDir = fileURLToPath(new URL('./runtime/extends', import.meta.url))
     nuxt.options._layers.push({
       cwd: themeDir,
-      config: { rootDir: themeDir, srcDir: themeDir, }
+      config: { rootDir: themeDir, srcDir: themeDir },
     } as any)
 
     const logger = useLogger('nuxt-web-bundle')
@@ -55,20 +58,25 @@ export default defineNuxtModule<ModuleOptions>({
         builder.setPrimaryURL(options.primaryURL || options.baseURL)
 
         const files = await globby('**/*', {
-          cwd: resolve(nuxt.options.rootDir, 'dist')
+          cwd: resolve(nuxt.options.rootDir, 'dist'),
         })
 
         for (const [index, file] of files.sort().reverse().entries()) {
           const content = await fsp.readFile(resolve(nuxt.options.rootDir, 'dist', file))
           const headers = {
             'Content-Type': mime.getType(basename(file)) || 'application/octet-stream',
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
           }
-          const url = new URL(file, options.baseURL).toString().replace(/\/index.html$/, '/').replace(/\.html$/, '')
+          const url = new URL(file, options.baseURL)
+            .toString()
+            .replace(/\/index.html$/, '/')
+            .replace(/\.html$/, '')
           builder.addExchange(url, 200, headers, content)
 
           let dir = dirname(url)
-          if (dir === '.') { dir = '' }
+          if (dir === '.') {
+            dir = ''
+          }
           const { pathname } = parseURL(url)
           const treeChar = index === files.length - 1 ? '└─' : '├─'
 
@@ -77,7 +85,10 @@ export default defineNuxtModule<ModuleOptions>({
         }
 
         const buf = builder.createBundle()
-        await fsp.writeFile(resolve(nuxt.options.rootDir, 'dist', options.filename), Buffer.from(buf, buf.byteOffset, buf.byteLength))
+        await fsp.writeFile(
+          resolve(nuxt.options.rootDir, 'dist', options.filename),
+          Buffer.from(buf, buf.byteOffset, buf.byteLength)
+        )
         const url = pathToFileURL(resolve(nuxt.options.rootDir, 'dist', options.filename))
         logger.success(`Bundle generated at \`${url}\`.`)
       })
--- src/runtime/extends/app/router.options.ts
+++ src/runtime/extends/app/router.options.ts
@@ -1,8 +1,8 @@
 import type { RouterOptions } from '@nuxt/schema'
 import { createMemoryHistory } from 'vue-router'
 
-export default <RouterOptions> {
-  history: (baseURL) => {
+export default <RouterOptions>{
+  history: baseURL => {
     return createMemoryHistory(baseURL)
-  }
+  },
 }
